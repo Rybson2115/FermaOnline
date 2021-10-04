65800592352dafcb8aef0204f557cf7fd9399c2c
K-Matecki <Kamil.wojciech.matecki@gmail.com>
2021-10-04 20:17:26 +0200

    zmiany widoków, naprawienie chart, dodanie template curve

--- FermaOnline/Models/TemplateCurve.cs
+++ FermaOnline/Models/TemplateCurve.cs
@@ -7,8 +7,10 @@ namespace FermaOnline.Models
     public static class TemplateCurve
     {
 
-        public static Dictionary<int, float> DefaultValue = new Dictionary<int, float>() { { 28,6.4f}, { 31, 8.4f },
-            { 35, 10.8f }, { 38, 14f }, { 46, 17.5f }, { 52, 21f }, { 60, 25f },{ 67, 8.4f } };//int-Dzień życia float-Masa ciała
+        public static Dictionary<int, float> DefaultValuePig = new Dictionary<int, float>() { { 21,5.0f} , { 28,6.6f}, { 35, 8.5f },
+            { 42,11.0f }, { 49, 14.2f }, { 56, 18.2f }, { 63, 22.8f }, { 70, 28.2f },{ 77, 34.4f } ,{ 84, 40.7f }, { 91, 47.2f },
+            { 98, 53.8f },{ 105, 60.5f },{ 112, 67.3f }, { 119, 74.2f },{ 126, 81.3f },{ 133, 88.8f },{ 140, 96.2f },{ 147, 103.4f },
+            { 154, 110.5f}, { 161, 117.5f },{ 168, 124.3f },{ 175, 130.9f},{ 182, 137.3f } };//int-Dzień życia float-Masa ciała
         public static List<float> LiveTimeValue { get; set; } //dane wyliczane na podstawie średniej z doświadczeń 
 
     }
